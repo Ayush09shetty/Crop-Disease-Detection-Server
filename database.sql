d7446a469085a5162284fb9192da125d1f6b3ace
Ayush Shetty <shettyayush5959@gmail.com>
2025-04-16 17:47:00 +0530

    Panel 3

--- database.sql
+++ database.sql
@@ -586,4 +586,214 @@ END $$
 
 DELIMITER ;
 
+-- CREATE TABLE diseases (
+--     id UUID PRIMARY KEY,
+--     name VARCHAR(100) UNIQUE NOT NULL,
+--     cure TEXT,
+--     precaution TEXT,
+--     causes TEXT
+-- );
+
+-- Create Disease-Product junction table
+CREATE TABLE diseases (
+    id CHAR(36) PRIMARY KEY,
+    name VARCHAR(100) UNIQUE NOT NULL,
+    cure TEXT,
+    precaution TEXT,
+    causes TEXT
+);
+CREATE TABLE disease_products (
+    id CHAR(36) PRIMARY KEY,
+    disease_id CHAR(36),
+    product_id CHAR(36),
+    FOREIGN KEY (disease_id) REFERENCES diseases(id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES productmodule_product(id) ON DELETE CASCADE
+);
+CREATE TABLE disease_consultants (
+    id CHAR(36) PRIMARY KEY,
+    disease_id CHAR(36),
+    consultant_id CHAR(36),
+    FOREIGN KEY (disease_id) REFERENCES diseases(id) ON DELETE CASCADE,
+    FOREIGN KEY (consultant_id) REFERENCES consultant_consultantuser(id) ON DELETE CASCADE
+);
+
+-- 1. Potato Early Blight
+INSERT INTO diseases (id, name, cure, precaution, causes) VALUES (
+    '11111111-1111-1111-1111-111111111111',
+    'Potato Early Blight',
+    'Fungicides like Mancozeb',
+    'Avoid overhead irrigation',
+    'Alternaria solani fungus'
+);
+
+INSERT INTO disease_products (id, disease_id, product_id) VALUES (
+    'dprod-0001-0001-0001-000000000001',
+    '11111111-1111-1111-1111-111111111111',
+    '03c079bb69c84a6883df79f1a892a56a'
+);
+
+INSERT INTO disease_consultants (id, disease_id, consultant_id) VALUES (
+    'dcons-0001-0001-0001-000000000001',
+    '11111111-1111-1111-1111-111111111111',
+    'd1e50c9ad47e470f88e0ac2a5887d9c4'
+);
+
+-- 2. Potato Healthy
+INSERT INTO diseases (id, name, cure, precaution, causes) VALUES (
+    '22222222-2222-2222-2222-222222222222',
+    'Potato Healthy',
+    'No treatment needed',
+    'Regular monitoring',
+    'No disease present'
+);
+
+INSERT INTO disease_products (id, disease_id, product_id) VALUES (
+    'dprod-0002-0002-0002-000000000002',
+    '22222222-2222-2222-2222-222222222222',
+    '077e47c82051400fa0dc8f8b16c0d985'
+);
+
+INSERT INTO disease_consultants (id, disease_id, consultant_id) VALUES (
+    'dcons-0002-0002-0002-000000000002',
+    '22222222-2222-2222-2222-222222222222',
+    'a1d9a0e1577748b4af9266a0f5c5a775'
+);
+
+-- 3. Potato Late Blight
+INSERT INTO diseases (id, name, cure, precaution, causes) VALUES (
+    '33333333-3333-3333-3333-333333333333',
+    'Potato Late Blight',
+    'Copper-based fungicides',
+    'Avoid overcrowding',
+    'Phytophthora infestans'
+);
+
+INSERT INTO disease_products (id, disease_id, product_id) VALUES (
+    'dprod-0003-0003-0003-000000000003',
+    '33333333-3333-3333-3333-333333333333',
+    '988d7e326a194fab88599348957c2600'
+);
+
+INSERT INTO disease_consultants (id, disease_id, consultant_id) VALUES (
+    'dcons-0003-0003-0003-000000000003',
+    '33333333-3333-3333-3333-333333333333',
+    '642f735e1a684756b4a356e5ea65f7d6'
+);
+
+-- 4. Tomato Bacterial Spot
+INSERT INTO diseases (id, name, cure, precaution, causes) VALUES (
+    '44444444-4444-4444-4444-444444444444',
+    'Tomato Bacterial Spot',
+    'Copper sprays',
+    'Use disease-free seeds',
+    'Xanthomonas spp.'
+);
+
+INSERT INTO disease_products (id, disease_id, product_id) VALUES (
+    'dprod-0004-0004-0004-000000000004',
+    '44444444-4444-4444-4444-444444444444',
+    '687ef43d54154591b8db20f164c24b41'
+);
+INSERT INTO disease_products (id, product_id, disease_id) VALUES
+('01f062d2-2a84-46aa-b87c-d24b7a66a084', '23a7fd81-8aa1-4a5d-b8b0-bbf169bee43e', '11111111-1111-1111-1111-111111111111'),
+('7d0c27f1-4461-48c3-96b5-9bb636f7156f', '3e4fca81-e496-422b-85f9-8a03efa41ec7', '22222222-2222-2222-2222-222222222222'),
+('2b93f25e-7333-4071-9e3b-647fa1df12a0', '4604d1e0-50d9-408e-b1db-5e6b4cf3b7b3', '33333333-3333-3333-3333-333333333333'),
+('c1dfeab5-eaa6-48e4-b505-e81c7dcfbe92', '6c628153-1bbc-41ec-80cd-a1f456c8ac48', '44444444-4444-4444-4444-444444444444'),
+('aa85ed1a-f364-4701-80c5-c2f857de5a8f', '86e7e4de-ba8b-4f8a-89e1-0d608c9b0e5f', '55555555-5555-5555-5555-555555555555'),
+('edf58755-b5ce-4fcb-a490-38fc82f2de1b', 'a4a730d4-af02-4845-a4ef-93a3ed630168', '66666666-6666-6666-6666-666666666666'),
+('77b055aa-9674-4a52-b3e3-2087ac0ea9c5', 'a986727e-1c34-4495-aa4d-cca9d4b211f3', '77777777-7777-7777-7777-777777777777'),
+('fdf672c1-3f29-49cd-9b70-e22c6c682962', 'aad925cf-809a-4ab8-816f-2ef0783bae1c', '88888888-8888-8888-8888-888888888888');
+
+
+
+INSERT INTO disease_consultants (id, disease_id, consultant_id) VALUES (
+    'dcons-0004-0004-0004-000000000004',
+    '44444444-4444-4444-4444-444444444444',
+    'd1e50c9ad47e470f88e0ac2a5887d9c4'
+);
+
+-- 5. Tomato Early Blight
+INSERT INTO diseases (id, name, cure, precaution, causes) VALUES (
+    '55555555-5555-5555-5555-555555555555',
+    'Tomato Early Blight',
+    'Apply fungicides like chlorothalonil',
+    'Crop rotation and sanitation',
+    'Alternaria solani'
+);
+
+INSERT INTO disease_products (id, disease_id, product_id) VALUES (
+    'dprod-0005-0005-0005-000000000005',
+    '55555555-5555-5555-5555-555555555555',
+    '03c079bb69c84a6883df79f1a892a56a'
+);
+
+INSERT INTO disease_consultants (id, disease_id, consultant_id) VALUES (
+    'dcons-0005-0005-0005-000000000005',
+    '55555555-5555-5555-5555-555555555555',
+    'a1d9a0e1577748b4af9266a0f5c5a775'
+);
+
+-- 6. Tomato Healthy
+INSERT INTO diseases (id, name, cure, precaution, causes) VALUES (
+    '66666666-6666-6666-6666-666666666666',
+    'Tomato Healthy',
+    'No treatment needed',
+    'Healthy cultural practices',
+    'No disease present'
+);
+
+INSERT INTO disease_products (id, disease_id, product_id) VALUES (
+    'dprod-0006-0006-0006-000000000006',
+    '66666666-6666-6666-6666-666666666666',
+    '077e47c82051400fa0dc8f8b16c0d985'
+);
+
+INSERT INTO disease_consultants (id, disease_id, consultant_id) VALUES (
+    'dcons-0006-0006-0006-000000000006',
+    '66666666-6666-6666-6666-666666666666',
+    '642f735e1a684756b4a356e5ea65f7d6'
+);
+
+-- 7. Tomato Late Blight
+INSERT INTO diseases (id, name, cure, precaution, causes) VALUES (
+    '77777777-7777-7777-7777-777777777777',
+    'Tomato Late Blight',
+    'Fungicides like metalaxyl',
+    'Remove infected leaves',
+    'Phytophthora infestans'
+);
+
+INSERT INTO disease_products (id, disease_id, product_id) VALUES (
+    'dprod-0007-0007-0007-000000000007',
+    '77777777-7777-7777-7777-777777777777',
+    '988d7e326a194fab88599348957c2600'
+);
+
+INSERT INTO disease_consultants (id, disease_id, consultant_id) VALUES (
+    'dcons-0007-0007-0007-000000000007',
+    '77777777-7777-7777-7777-777777777777',
+    'd1e50c9ad47e470f88e0ac2a5887d9c4'
+);
+
+-- 8. Tomato Septoria Spot
+INSERT INTO diseases (id, name, cure, precaution, causes) VALUES (
+    '88888888-8888-8888-8888-888888888888',
+    'Tomato Septoria Spot',
+    'Use chlorothalonil fungicide',
+    'Space plants properly',
+    'Septoria lycopersici'
+);
+
+INSERT INTO disease_products (id, disease_id, product_id) VALUES (
+    'dprod-0008-0008-0008-000000000008',
+    '88888888-8888-8888-8888-888888888888',
+    '687ef43d54154591b8db20f164c24b41'
+);
+
+INSERT INTO disease_consultants (id, disease_id, consultant_id) VALUES (
+    'dcons-0008-0008-0008-000000000008',
+    '88888888-8888-8888-8888-888888888888',
+    'a1d9a0e1577748b4af9266a0f5c5a775'
+);
+
 
